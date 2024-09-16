c8373031fa53ae8562a4738ef61c6e4b5a29ed5d
dcm1302 <dcmonave@gmail.com>
2024-09-16 07:18:52 -0500

    Added controller

---
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class ProductoController {
+
+}
