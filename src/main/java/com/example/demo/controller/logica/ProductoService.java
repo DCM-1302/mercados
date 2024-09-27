a7299a7d6f8eb7604ec0d4f7f64e62dafce77322
dcm1302 <dcmonave@gmail.com>
2024-09-26 21:53:40 -0500

    commit

--- src/main/java/com/example/demo/controller/logica/ProductoService.java
+++ src/main/java/com/example/demo/controller/logica/ProductoService.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller.logica;
 
+import com.example.demo.db.jpa.OrdenJPA;
 import com.example.demo.db.jpa.ProductoJPA;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class ProductoService {
 
+ private final OrdenJPA ordenJPA;
  private final ProductoJPA productoJPA;
 }
