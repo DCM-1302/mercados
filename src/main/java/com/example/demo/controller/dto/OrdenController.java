6850411756c1dd67b061f8324add443132d45438
dcm1302 <dcmonave@gmail.com>
2024-09-29 17:27:40 -0500

    commit

--- src/main/java/com/example/demo/controller/dto/OrdenController.java
+++ src/main/java/com/example/demo/controller/dto/OrdenController.java
@@ -3,11 +3,13 @@ package com.example.demo.controller.dto;
 import com.example.demo.controller.logica.OrdenService;
 import com.example.demo.db.orm.OrdenORM;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 public class OrdenController {
@@ -23,6 +25,7 @@ public class OrdenController {
     @GetMapping(path= "/ventas")
     public List<OrdenORM> consultarOrdenesPorDia(@RequestParam String fecha){
         LocalDate fechaVenta = LocalDate.parse(fecha);
+        log.info("Fecha de venta: " + fechaVenta);
         return ordenService.consultarOrden(fechaVenta);
     }
 }
--- src/test/java/com/example/demo/controller/integration/IntegrationTest.java
+++ src/test/java/com/example/demo/controller/integration/IntegrationTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller.integration;
 
+import com.example.demo.controller.dto.OrdenDTO;
 import com.example.demo.controller.dto.ProductoDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
@@ -18,13 +20,26 @@ public class IntegrationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
     @Test
     void TestProductoController() {
         ProductoDTO nuevoProducto = new ProductoDTO("Giorno","Giovanna",5000,1L,10,"XS","Passione" );
         ResponseEntity<String> respuestaInsercion = restTemplate.postForEntity("/producto",nuevoProducto, String.class);
         System.out.println(respuestaInsercion);
-        Assertions.assertEquals("Cancion guardada", respuestaInsercion.getBody());
+        Assertions.assertEquals("Producto guardado", respuestaInsercion.getBody());
         ResponseEntity<List> resultado = restTemplate.getForEntity("/mercancia", List.class);
+        Assertions.assertFalse(Objects.requireNonNull(resultado.getBody()).isEmpty());
+        Assertions.assertTrue(resultado.getStatusCode().is2xxSuccessful());
+
+    }
+
+    @Test
+    void TestOrdenController() {
+        OrdenDTO nuevaOrden = new OrdenDTO(1L, LocalDate.now(),10,3000);
+        ResponseEntity<String> respuestaInsercion = restTemplate.postForEntity("/registrar", nuevaOrden, String.class);
+        System.out.println(respuestaInsercion);
+        Assertions.assertEquals("Orden guardada", respuestaInsercion.getBody());
+        ResponseEntity<List> resultado = restTemplate.getForEntity("/ventas?fecha=2024-09-29", List.class);
         Assertions.assertFalse(Objects.requireNonNull(resultado.getBody()).isEmpty());
         Assertions.assertTrue(resultado.getStatusCode().is2xxSuccessful());
 
