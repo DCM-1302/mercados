2acd92d3050d04e7a4b0481f772edbf079bcbcdf
dcm1302 <dcmonave@gmail.com>
2024-09-28 20:24:42 -0500

    commit

--- src/main/java/com/example/demo/controller/dto/OrdenController.java
+++ src/main/java/com/example/demo/controller/dto/OrdenController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller.dto;
 
 import com.example.demo.controller.logica.OrdenService;
+import com.example.demo.db.orm.OrdenORM;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -12,6 +16,13 @@ public class OrdenController {
 
     @PostMapping(path="/registrar")
     public String guardarOrden(@RequestBody OrdenDTO ordenDTO){
+        ordenService.guardarOrden(ordenDTO.producto(), ordenDTO.Cantidad());
         return "Venta registrada";
+    }
+
+    @GetMapping(path= "/ventas")
+    public List<OrdenORM> consultarOrdenesPorDia(@RequestParam String fecha){
+        LocalDate fechaVenta = LocalDate.parse(fecha);
+        return ordenService.consultarOrden(fechaVenta);
     }
 }
