5d3c47773680134b4e77c0355e7d63bb7bec890c
dcm1302 <dcmonave@gmail.com>
2024-09-29 16:31:34 -0500

    commit

--- src/test/java/com/example/demo/controller/integration/IntegrationTest.java
+++ src/test/java/com/example/demo/controller/integration/IntegrationTest.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller.integration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 }
